eef57feb67793c73c3050b726843e7d0384d3f5d
ssoon <jjinimania@gmail.com>
2018-03-04 16:53:00 +0900

    Enum

--- ReplaceMagicNumberWithSymbolicConstant/src/main/java/me/ssoon/Main.java
+++ ReplaceMagicNumberWithSymbolicConstant/src/main/java/me/ssoon/Main.java
@@ -1,11 +1,13 @@
 package me.ssoon;
 
+import me.ssoon.Robot.Command;
+
 public class Main {
 
   public static void main(String[] args) {
     final Robot robot = new Robot("Andrew");
-    robot.order(Robot.COMMAND_WALK);
-    robot.order(Robot.COMMAND_STOP);
-    robot.order(Robot.COMMAND_JUMP);
+    robot.order(Command.WALK);
+    robot.order(Command.STOP);
+    robot.order(Command.JUMP);
   }
 }
--- ReplaceMagicNumberWithSymbolicConstant/src/main/java/me/ssoon/Robot.java
+++ ReplaceMagicNumberWithSymbolicConstant/src/main/java/me/ssoon/Robot.java
@@ -7,16 +7,22 @@ public class Robot {
   public static final RobotCommand COMMAND_JUMP = new RobotCommand("JUMP");
   private final String name;
 
+  public enum Command {
+    WALK,
+    STOP,
+    JUMP
+  }
+
   public Robot(final String name) {
     this.name = name;
   }
 
-  public void order(final RobotCommand command) {
-    if (command == COMMAND_WALK) {
+  public void order(final Robot.Command command) {
+    if (command == Command.WALK) {
       System.out.println(name + " walks.");
-    } else if (command == COMMAND_STOP) {
+    } else if (command == Command.STOP) {
       System.out.println(name + " stops.");
-    } else if (command == COMMAND_JUMP) {
+    } else if (command == Command.JUMP) {
       System.out.println(name + " jumps.");
     } else {
       System.out.println("Command error. command = " + command);
