ae8edc2740d080771ac539bae4d962c2b44f0dd6
ssoon <jjinimania@gmail.com>
2018-03-04 17:19:56 +0900

    RemoveControlFlag > findInt boolean 제거

--- RemoveControlFlag/src/main/java/me/ssoon/findint/FindInt.java
+++ RemoveControlFlag/src/main/java/me/ssoon/findint/FindInt.java
@@ -3,12 +3,11 @@ package me.ssoon.findint;
 public class FindInt {
 
   public static boolean find(final int[] data, final int target) {
-    boolean flag = false;
-    for (int i = 0; i < data.length && !flag; i++) {
+    for (int i = 0; i < data.length; i++) {
       if (data[i] == target) {
-        flag = true;
+        return true;
       }
     }
-    return flag;
+    return false;
   }
 }
